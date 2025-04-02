f7727d38a919366ecf2f89829d38b50520f97b16
Woodlley <andrew.woolley@student.manchester.ac.uk>
2025-04-02 19:57:17 +0000

    moon plotting still bad data

--- moon.py
+++ moon.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 # Define Moon parameters
 R_moon = 1737e3  # Radius of the Moon (m)
@@ -15,8 +16,8 @@ thickness = 1  # Thickness of the model (m)
 N_lay = 10  # Number of layers
 N_lat = 25  # Latitude divisions
 N_lon = 50  # Longitude divisions
-N_days = 10  # Number of moon days
-N_time = N_days*240  # number of times steps
+N_days = 5  # Number of moon days
+N_time = N_days * 240  # number of times steps
 dt = 2.73 * 3600  # Time step (s) 1/240 moon days
 # dt = day_length / 200  # Time step (sec)
 # print(dt)
@@ -37,8 +38,10 @@ Th, Ph = np.meshgrid(theta, phi) # returns shape (len(phi), len(theta))
 # print(Ph)
 # Initialize temperature array
 T = 250 * np.ones_like(Th)  # Initial temperature (K)
-T = np.stack([T] * N_lay, axis=0)  # Stack layers
-
+# T = np.stack([T] * (N_lay-1), axis=0)  # Stack layers
+surf_T = 100 * np.cos(Th) * np.sin(Ph) + 250  # Surface temperature (K)
+T = np.stack((surf_T, T, T, T, T, T, T, T, T, T), axis=0)  # Add surface layer
+# print(T[-1])
 # Define solar flux (simplified)
 S_max = 1360.9  # Solar radiation at 1 AU (W/m^2)
 emissivity = 0.95
@@ -51,12 +54,14 @@ for t in range(N_time):
     print(t)
     # Approximate Laplacian using finite difference
     T_new = T.copy()
-    for l in range (0, N_lay-1):
+    for l in range (0, N_lay):
         for i in range(0, N_lat):
             for j in range(0, N_lon):
                 #1D laplacian
                 if l==0:
                     laplacian_T = (0 + T[1, i, j] - 2*T[l, i, j]) / dr**2
+                elif l == N_lay-1:
+                    laplacian_T = (T[l-1, i, j] + T[l, i, j] - 2*T[l, i, j]) / dr**2
                 else:
                     laplacian_T = (T[l+1, i, j] + T[l-1, i-1, j] - 2*T[l, i, j]) / dr**2
                 # grad_T = np.sqrt((T[l, i+1, j] - T[l, i-1, j])**2 + (T[l, i, j+1] - T[l, i, j-1])**2) / (2 * dx)
@@ -70,9 +75,10 @@ for t in range(N_time):
                     # print(S)
                     # print(Ph[i, j])
                         T_new[l, i, j] += (S * dt) / (rho * Cp * thickness)  # Heat absorbed
-
+                        print(S * dt / (rho * Cp * thickness))
                     # Radiative cooling (Stefan-Boltzmann law)
                     T_new[l, i, j] -= emissivity * sigma * T[l, i, j]**4 * dt / (rho * Cp * thickness)
+                    print(emissivity * sigma * T[l, i, j]**4 * dt / (rho * Cp * thickness))
                 T_new[l, i, j] = max(min(T_new[l, i, j], 500), -100)  # Keep within realistic lunar temperature limits
     
     # print(T_new[1, 1])
@@ -87,13 +93,71 @@ for t in range(N_time):
     T = T_new.copy()
 # print(T)
 # Plot results
-plt.imshow(T[0], cmap='hot', aspect='auto')
-plt.colorbar(label='Temperature (K)')
-plt.title("Approximate Lunar Surface Temperature Distribution")
-plt.xlabel("Longitude")
-plt.ylabel("Latitude")
-plt.savefig("moon_temperature.png", dpi=300)
+temps = T[0, :, :] - 273  # Surface temperature (^oC)
+shape = temps.shape
+min_temp = temps.min()
+max_temp = temps.max()
+
+# Use below to make sphere same shape as temps, 
+# Assuming axis 0 is phi (0 -> pi)
+# axis 1 is theta (0 -> 2pi)
+# If not, swap 0 & 1 below and the creation of x & y
+
+# # sphere
+phi = np.linspace(0, np.pi, shape[0])
+theta = np.linspace(0, 2*np.pi, shape[1])
+T, P = np.meshgrid(theta, phi) # returns shape (len(phi), len(theta))
+r = 1
+
+# #cartesian coordinates
+x = r * np.sin(P) * np.cos(T)
+y = r * np.sin(P) * np.sin(T)
+z = r * np.cos(P)
+
+colour_map = mpl.colormaps['RdYlBu_r'] # Red to yellow to blue colour map
+
+perf_temp = 25 # chosen temperature for the cheese
+
+# Sets colour map to centre on perf_temp
+if np.abs(perf_temp-max_temp) >= np.abs(perf_temp-min_temp):
+    norm = mpl.colors.Normalize(vmin=(2*perf_temp)-max_temp, vmax=max_temp)
+elif min_temp < 0:
+    norm = mpl.colors.Normalize(vmin=min_temp, vmax=(2*perf_temp)-min_temp)
+
+colours = colour_map(norm(temps))
+
+
+# Plotting moon
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+surface = ax.plot_surface(x, y, z, norm=norm, edgecolor=None, linewidth=0.5, cmap=colour_map, facecolors=colours)
+ax.set_axis_off()
+# Adding colourbar
+cbar = plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=colour_map), ax=ax)
+cbar.set_label('Temperature (°C)')
+if np.abs(perf_temp-max_temp) >= np.abs(perf_temp-min_temp):  # Changes colourbar axis to match above
+    cbar.set_ticks([(2*perf_temp)-max_temp, min_temp, -100, -50, 0, perf_temp, 50, 100, max_temp], labels=['{:.0f}'.format((2*perf_temp)-max_temp), '{:.0f}'.format(min_temp), '-100', '-50', '0', 'perfect', '50', '100', '{:.0f}'.format(max_temp)])
+elif min_temp < 0:
+    cbar.set_ticks([min_temp, -100, -50, 0, perf_temp, 50, 100, max_temp, (2*perf_temp)-min_temp], labels=['{:.0f}'.format(min_temp), '-100', '-50', '0', 'perfect', '50', '100', '{:.0f}'.format(max_temp), '{:.0f}'.format((2*perf_temp)-min_temp)])
+# In set_ticks 
+# First list is value positions, second is labels
+# Indices have to match
+
+
+ax.set_title('{:.3f} $pi$ resolution'.format(1 / shape[0]))
+ax.set_box_aspect((1, 1, 1)) # to be regular sphere
+ax.view_init(30, 0, 0) # Change viewing angle https://matplotlib.org/stable/api/_as_gen/mpl_toolkits.mplot3d.axes3d.Axes3D.view_init.html#mpl_toolkits.mplot3d.axes3d.Axes3D.view_init
+plt.savefig('moon.png', dpi=600, transparent=True)
 plt.show()
-plt.plot(T[0, 12, :])
-plt.savefig("moon_temperature_profile.png", dpi=300)
+
+eq_index = shape[0] // 2 # Find equator index of temps
+
+# Plot temps across equator
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+ax1.plot(theta/np.pi, temps[eq_index], c='k')
+ax1.spines[["left", "bottom"]].set_position(("data", 0)) # Puts x axis at y=0, can remove
+ax1.spines[["top", "right"]].set_visible(False)
+plt.savefig('moon_eq.png', dpi=600, transparent=True)
 plt.show()
+
